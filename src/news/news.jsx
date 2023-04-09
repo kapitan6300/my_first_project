e5483adfcc91451b78736d55b51f3fbdd6e8ef12
Kep <shafiev6300@gmail.com>
2023-04-09 13:08:20 +0700

    add new chanes

--- src/news/news.jsx
+++ src/news/news.jsx
@@ -1,27 +1,5 @@
-import axios from "axios";
-import React from "react";
-import classes from './news.module.css';
 
-const News = (props) => {
-  axios.get('https://habrahabr.ru/rss/interesting').then(response=>{
-    props.setUsers(response.data.items)
-  })
- debugger;
-    return (
-<div className={classes.kadu}>
-    {props.news.map(n =><div name={n.name} news={n.news}>
-        <div>
-            {n.status} <br/>
-            {n.name}
-        </div>
-    </div>)}
-    
-    
-</div>
-    )
-    
-    
-    
-    
-}
-export default News;
+import React from "react";
+//import classes from './news.module.css';
+  fetch('https://hn.algolia.com/api/v1/items/4')
+  .then(resp=>console.log(resp))
